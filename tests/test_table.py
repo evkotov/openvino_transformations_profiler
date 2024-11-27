bd58940bad212e55666c160fdd8423ce43117a33
Evgeny Kotov <evgeny.kotov@intel.com>
2024-11-27 16:26:56 +0100

    add unit tests

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,5 +1,5 @@
 import unittest
-from table import get_comparison_values, compare_compile_time
+from table import get_comparison_values, compare_compile_time, sort_table
 from common_structs import ModelInfo, ModelData
 
 
@@ -28,6 +28,34 @@ class TestTableFunctions(unittest.TestCase):
         comparison_values = get_comparison_values(table, 'key1', 'key2', 'ms')
         self.assertEqual(comparison_values.values1, [1.0])
         self.assertEqual(comparison_values.values2, [2.0])
+
+
+class TestSortTable(unittest.TestCase):
+
+    def test_with_valid_float_keys(self):
+        table = [{'value': 1.0}, {'value': 3.0}, {'value': 2.0}]
+        sorted_table = sort_table(table, lambda row: row['value'])
+        self.assertEqual(sorted_table, [{'value': 3.0}, {'value': 2.0}, {'value': 1.0}])
+
+    def test_with_mixed_type_keys(self):
+        table = [{'value': 1.0}, {'value': 'invalid'}, {'value': 2.0}]
+        sorted_table = sort_table(table, lambda row: row['value'])
+        self.assertEqual(sorted_table, [{'value': 2.0}, {'value': 1.0}, {'value': 'invalid'}])
+
+    def test_with_all_invalid_keys(self):
+        table = [{'value': 'invalid1'}, {'value': 'invalid2'}, {'value': 'invalid3'}]
+        sorted_table = sort_table(table, lambda row: row['value'])
+        self.assertEqual(sorted_table, [{'value': 'invalid1'}, {'value': 'invalid2'}, {'value': 'invalid3'}])
+
+    def test_empty_table(self):
+        table = []
+        sorted_table = sort_table(table, lambda row: row.get('value', 0.0))
+        self.assertEqual(sorted_table, [])
+
+    def test_with_none_keys(self):
+        table = [{'value': None}, {'value': 2.0}, {'value': 1.0}]
+        sorted_table = sort_table(table, lambda row: row['value'] if row['value'] is not None else 0.0)
+        self.assertEqual(sorted_table, [{'value': 2.0}, {'value': 1.0}, {'value': None}])
 
 
 if __name__ == '__main__':
